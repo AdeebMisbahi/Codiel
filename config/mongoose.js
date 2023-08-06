4116b9e6468c0d93f3f76f27d35302b9f2226a46
AdeebMisbahi1 <130211733+AdeebMisbahi@users.noreply.github.com>
2023-08-06 17:30:38 +0530

    all set for sign in sign up page

--- config/mongoose.js
+++ config/mongoose.js
@@ -8,7 +8,7 @@ const db=mongoose.connection;
 db.on('error', console.error.bind(console, "Error connecting to Mongodb"));
 
 db.once('open', function(){
-    console.log('Connected to Database MongoDB');
+    console.log('Connected to Database :: MongoDB');
 });
 
 
