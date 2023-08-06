13221709ff0aad268796a0d0cbd53c75182d2075
AdeebMisbahi1 <130211733+AdeebMisbahi@users.noreply.github.com>
2023-08-06 11:37:09 +0530

    set up sign in and sign in page

--- index.js
+++ index.js
@@ -2,8 +2,26 @@
 const express=require('express');
 // app will run on this port
 const port=3000;
-
 const app=express();
+// acquire mongodb database
+const db=require('./config/mongoose')
+// require expres-ejs-layout
+const expressLayouts=require('express-ejs-layouts');
+// access static file
+app.use(express.static('./assets'));
+// and then use express layouts
+app.use(expressLayouts);
+// extract style and scripts from sub pages into the layout 
+app.set('layout extractStyles', true);
+app.set('layout extractScripts', true);
+
+// use express router
+app.use('/', require('./routes'));
+
+// set up the view engine
+app.set('view engine', 'ejs');
+app.set('views', './views')
+
 
 
 app.listen(port, function(err){
